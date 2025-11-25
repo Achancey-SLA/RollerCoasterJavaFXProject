06b92e0d710aeaec91d454131cdef1e21ba9a539
alexchancey <achancey27@scienceleadership.org>
2025-11-25 11:45:21 -0500

    added GoodCoasterData

--- src/main/java/com/example/rollercoasterjavafxproject/CoasterController.java
+++ src/main/java/com/example/rollercoasterjavafxproject/CoasterController.java
@@ -3,9 +3,19 @@ package com.example.rollercoasterjavafxproject;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
+import java.io.IOException;
+
 public class CoasterController {
     @FXML
     private Label welcomeText;
+    public void initialize() throws Exception {
+        GoodCoaster.readGoodData();
+        TallCoaster.readTallCoasterData();
+
+        for (RollerCoaster eachCoaster: RollerCoaster.getCoastersList()){
+            System.out.println(eachCoaster);
+        }
+    }
 
     @FXML
     protected void onHelloButtonClick() {
