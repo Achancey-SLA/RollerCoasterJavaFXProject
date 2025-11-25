f24e4189851e699746375f668392fb2b5bdad699
alexchancey <achancey27@scienceleadership.org>
2025-11-25 11:32:47 -0500

    renamed files

--- src/main/java/com/example/rollercoasterjavafxproject/CoasterController.java
+++ src/main/java/com/example/rollercoasterjavafxproject/CoasterController.java
@@ -3,7 +3,7 @@ package com.example.rollercoasterjavafxproject;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class CoasterController {
     @FXML
     private Label welcomeText;
 
