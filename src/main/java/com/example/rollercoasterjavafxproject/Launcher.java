f178f811dd66237a7c9acec8df49bc226b21de13
MinghaoZhang <mzhang26@scienceleadership.org>
2025-12-01 13:22:00 -0500

    Scenebuilder updates

--- src/main/java/com/example/rollercoasterjavafxproject/Launcher.java
+++ src/main/java/com/example/rollercoasterjavafxproject/Launcher.java
@@ -2,7 +2,7 @@ package com.example.rollercoasterjavafxproject;
 
 import javafx.application.Application;
 
-public class Launcher {
+public class        Launcher {
     public static void main(String[] args) {
         Application.launch(CoasterApplication.class, args);
     }
